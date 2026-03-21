4c5e474baba56e0f05b90e27e7b678dc9afb3bec
sagaro18 <108170289+sagaro18@users.noreply.github.com>
2026-03-21 15:44:35 +0900

    Add new script

--- apps/mobile/src/data/story.ts
+++ apps/mobile/src/data/story.ts
@@ -2,40 +2,158 @@ import { DialogueLine } from '../types';
 
 export const chapter1: DialogueLine[] = [
   {
-      id: "scene1_001",
-      speaker: "",
-      text: "<System Matrix Compromised>",
-      expression: "none",
-      background: "beginner_cave"
+      "id": "scene1_001",
+      "speaker": "",
+      "text":"<System Matrix Compromised>",
+      "expression": "none",
+      "background": "beginner_cave"
   },
   {
-      id: "scene1_002",
-      speaker: "",
-      text: "<System failure>",
-      expression: "none",
-      background: "beginner_cave"
+      "id": "scene1_002",
+      "speaker": "",
+      "text":"<System failure>",
+      "expression": "none",
+      "background": "beginner_cave"
   },
   {
-      id: "scene1_003",
-      speaker: "",
-      text: "A rumbling noise is heard in the background, you wake up dazed and confused inside a pod. Suddenly the mechanical noise is heard from under you a click is heard and your arm and leg restraints are released.",
-      expression: "none",
-      background: "beginner_cave"
+      "id": "scene1_003",
+      "speaker": "",
+      "text":"A rumbling noise is heard in the background, you wake up dazed and confused inside a pod. Suddenly the mechanical noise is heard from under you a click is heard and your arm and leg restraints are released.",
+      "expression": "none",
+      "background": "beginner_cave"
   },
   {
-      id: "scene4_002",
-      speaker: "Vaina",
-      text: "Oh good you're awake!",
-      expression: "Happy",
-      background: "Village_Interior"
+      "id": "scene1_004",
+      "speaker": "Player",
+      "text":"Wait? Why was I restrained?",
+      "expression": "none",
+      "background": "beginner_cave"
   },
   {
-      id: "scene4_008",
-      speaker: "",
-      text: "Like Vaina said, you seemed to be alright now and you decide to go out to the Village to see what it and the strange World has to offer",
-      expression: "none",
-      background: "Village_Interior",
-      triggerEvent: "START_FREE_TIME",
-      _comment: "End of Chapter 1"
+      "id": "scene1_005",
+      "speaker": "Player",
+      "text":"Actually who am I, what is this place and why am I here?",
+      "expression": "none",
+      "background": "beginner_cave"
+  },
+  {
+      "id": "scene1_006",
+      "speaker": "",
+      "text":"...",
+      "expression": "none",
+      "background": "beginner_cave",
+      "_comment": "This is where the alarm noise is heard"
+  },
+  {
+      "id": "scene1_007",
+      "speaker": "Player",
+      "text":"No time to be thinking about that now, I have to get out of here NOW",
+      "expression": "none",
+      "background": "beginner_cave",
+      "_comment": "This is where the alarm noise is heard"
+  },
+  {
+      "id": "scene1_008",
+      "speaker": "Player",
+      "text":"No time to be thinking about that now, I have to get out of here NOW",
+      "expression": "none",
+      "background": "beginner_cave",
+      "_comment": "This is where the alarm noise is heard"
+  },
+  {
+      "id": "scene2_001",
+      "speaker": "Player",
+      "text":"Just in the nick of time",
+      "expression": "none",
+      "background": "nature_1",
+      "_comment": "Switch to nature scene as explotions are heard in the background"
+  },
+  {
+      "id": "scene2_002",
+      "speaker": "",
+      "text":"Before you could gather your thoughts, you smell that air, feel the green grass and feel the sunlight that you haven't felt in a long time.",
+      "expression": "none",
+      "background": "nature_1",
+      "_comment": "nature sounds"
+  },
+  {
+      "id": "scene3_001",
+      "speaker": "",
+      "text":"As your hearing adjusts, you hear the footsteps of someone rushing towards you",
+      "expression": "none",
+      "background": "nature_2",
+      "_comment": "This scene is a shows a blurred figure running towards the player, running sounds"
+  },
+  {
+      "id": "scene3_002",
+      "speaker": "",
+      "text":"Before you can see this figure clearly, your consciousness fades...",
+      "expression": "none",
+      "background": "nature_2",
+      "_comment": "This scene is a shows a blurred figure running towards the player, running sounds"
+  },
+  {
+      "id": "scene4_001",
+      "speaker": "",
+      "text":"You wake up to the figure you saw previously, a strange glow emitting from her hands",
+      "expression": "none",
+      "background": "Village_Interior",
+      "_comment": "This scene is a shows the inside of a village house, probabaly the main cheifs house or something"
+  },
+  {
+      "id": "scene4_002",
+      "speaker": "Vaina",
+      "text":"Oh good you're awake!",
+      "expression": "Happy",
+      "background": "Village_Interior",
+      "_comment": ""
+  },
+  {
+      "id": "scene4_003",
+      "speaker": "Player",
+      "text":"Who are you?, Where are we?",
+      "expression": "",
+      "background": "Village_Interior",
+      "_comment": ""
+  },
+  {
+      "id": "scene4_004",
+      "speaker": "Vaina",
+      "text":"Quick to the questions eh Mister, I guess that means you're gonna be alright",
+      "expression": "Happy",
+      "background": "Village_Interior",
+      "_comment": ""
+  },
+  {
+      "id": "scene4_005",
+      "speaker": "Vaina",
+      "text":"I'm Vaina, the healer of the Village of the South, whatta'bout you Mister",
+      "expression": "Happy",
+      "background": "Village_Interior",
+      "_comment": ""
+  },
+  {
+      "id": "scene4_006",
+      "speaker": "Player",
+      "text":"I..Dont Remember",
+      "expression": "",
+      "background": "Village_Interior",
+      "_comment": ""
+  },
+  {
+      "id": "scene4_007",
+      "speaker": "Vaina",
+      "text":"Secretive are we Mister, We'll feel free to stay and look around our village for as long as you like, dontcha worry, we don't bite, unless you talk to Meri that is ",
+      "expression": "Gigling",
+      "background": "Village_Interior",
+      "_comment": ""
+  },
+  {
+      "id": "scene4_008",
+      "speaker": "",
+      "text":"Like Vaina said, you seemed to be alright now and you decide to go out to the Village to see what it and the strange World has to offer",
+      "expression": "",
+      "background": "Village_Interior",
+      "_comment": "End of Chapter 1"
   }
-];
+]
