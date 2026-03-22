fb25951074a98c1e94f6bbc35c75e53170d3a53c
sagaro18 <108170289+sagaro18@users.noreply.github.com>
2026-03-22 16:16:09 +0900

    Add background

---
+++ apps/mobile/src/data/assets.ts
@@ -0,0 +1,33 @@
+export const backgrounds = {
+    beginner_cave: require('../../assets/backgrounds/cave.png'),
+    nature_1: require('../../assets/backgrounds/world-overview.png'),
+    nature_2: require('../../assets/backgrounds/world-run.png'),
+    Village_Interior: require('../../assets/backgrounds/village-interior.png'),
+    Village_Exterior: require('../../assets/backgrounds/village-exterior-day.png'),
+    Village_Night: require('../../assets/backgrounds/village-night.png'),
+    Premium_Vaina: require('../../assets/backgrounds/premium-vaina.png'),
+    Premium_Meri: require('../../assets/backgrounds/premium-meri.png'),
+  } as const;
+  
+  export const portraits = {
+    Vaina: {
+      Angry: require('../../assets/characters/vaina/angry.png'),
+      Happy: require('../../assets/characters/vaina/happy.png'),
+      Neutral: require('../../assets/characters/vaina/default.png'),
+      Giggling: require('../../assets/characters/vaina/happy.png'),
+      Sad: require('../../assets/characters/vaina/sad.png'),
+      Shocked: require('../../assets/characters/vaina/shocked.png'),
+      none: require('../../assets/characters/vaina/default.png'),
+      default: require('../../assets/characters/vaina/default.png'),
+    },
+    Meri: {
+      Angry: require('../../assets/characters/meri/angry.png'),
+      Happy: require('../../assets/characters/meri/happy.png'),
+      Neutral: require('../../assets/characters/meri/happy.png'),
+      Giggling: require('../../assets/characters/meri/happy-2.png'),
+      Sad: require('../../assets/characters/meri/sad.png'),
+      Shocked: require('../../assets/characters/meri/shocked.png'),
+      none: require('../../assets/characters/meri/happy.png'),
+      default: require('../../assets/characters/meri/happy.png'),
+    },
+  } as const;
